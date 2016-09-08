8608d88ec89e50542c45ad07e844a7eaf5f3e470
jimmychan <chn_jin@163.com>
2016-09-08 22:13:42 +0800

    Initial Commit

---
+++ CoreData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreData
+//
+//  Created by jimmy Chan on 16/9/8.
+//  Copyright © 2016年 jimmychan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by jimmy Chan on 16/9/8.
+//  Copyright © 2016年 jimmychan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
